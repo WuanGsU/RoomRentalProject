e7c42169eda646232d0ca8a56fd0f14ec12394fe
Quang <21111061265@hunre.edu.vn>
2024-12-28 23:41:33 +0700

    Initial commit for RoomRental project

---
+++ src/main/java/com/quang/roomrental/RoomRentalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.quang.roomrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomRentalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomRentalProjectApplication.class, args);
+    }
+
+}
